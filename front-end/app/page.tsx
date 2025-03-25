9b1d777c0933162db79020a1585c965bc77b6dcc
IsabellyL-dev <isabelly.lmoura@gmail.com>
2025-03-25 15:05:59 -0300

    styles: fix login-screen style

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,8 +1,11 @@
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
+    <img className={styles.topImage}src="/images/elementTop.png" alt="" />
+    <img className={styles.bottomImage} src="/images/elementBottom1.png" alt="" />
     <section className={styles.loginSection}>
       <h1 className={styles.h1}>ACESSE SUA CONTA</h1>
       <div className={styles.divInput}>
@@ -14,26 +17,31 @@ export default function Home() {
 
 
       <div className={styles.divInput}>
-            <span  className={`material-symbols-outlined ${styles.icon}`}>
-            lock
-            </span>
-        <input className={styles.inputs} type="password" name="SenhaL" id="SenhaL" placeholder="Digite sua senha"/>
+      <span  className={`material-symbols-outlined ${styles.icon}`}>
+      lock
+      </span>
+      <input className={styles.inputs} type="password" name="SenhaL" id="SenhaL" placeholder="Digite sua senha"/>
       </div>
 
-      <div className={styles.checkbox}>
-        <p className={styles.checkbox} ><input className={styles.checkbox} type="checkbox" name="manterConectado" id="manterConectado"/>Continuar conectado</p>
-      </div>
+    <div className={styles.checkbox}>
+    <label>
+    <input className={styles.inputCheckbox} type="checkbox" name="manterConectado" id="manterConectado"/>
+    Continuar conectado
+    </label>
+    </div>
+
 
       <input className={styles.enter} type="button" value="ENTRAR" />
        <p className={styles.forgot} >Esqueceu sua senha? </p>
        <a className={styles.link} href="#">Clique aqui</a> 
-       <div className={styles.divider}>
-<span>Conecte-se também com:</span>
+
+<div className={styles.divider}>
+<div className={styles.lineDivider}><p className={styles.conect}>Conecte-se também com:</p></div>
 </div>
 
 <div className={styles.socialIcons}>
-<img src="../images/google.png" alt="Login com Google" />
-<img src="../images/microsoft.png" alt="Login com Microsoft" />
+<img src="/images/logoIesb.png" alt="Login com Google" />
+<img src="/images/microsoftIcon.png" alt="Login com Microsoft" />
 </div>
 
 <div className={styles.createAccount}>
