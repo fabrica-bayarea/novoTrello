981f40d0cec3d1f1942a46f19226b565e0e6d75f
IsabellyL-dev <isabelly.lmoura@gmail.com>
2025-03-26 10:10:19 -0300

    style: fix register screen style

--- front-end/app/register/page.tsx
+++ front-end/app/register/page.tsx
@@ -4,7 +4,7 @@ export default function register(){
         <main className={styles.main}>
             <img className={styles.logo} src="/images/logoIesb.png" alt="Logo IESB" />
             <section className={styles.registerSection}>
-                <h1>CRIE UMA CONTA</h1>
+                <h1 className={styles.title}>CRIE UMA CONTA</h1>
                 <div>
                     <input className={styles.divInput}type="text" name="name" id="name" placeholder="Digite seu nome completo" />
                 </div>
@@ -12,14 +12,24 @@ export default function register(){
                     <input className={styles.divInput} type="email" name="email" id="email" placeholder="Digite seu email" />
                 </div>
                 <div>
-                    <label>Senha</label>
-                    <input className={styles.divInput} type="password" name="password" id="password" placeholder="Digite sua senha" />
+                    <input className={styles.divInput} type="password" name="password" id="password" placeholder="Crie uma senha" />
+                    
+                </div>
+                <div className={styles.divText}>
+                <p className={styles.text}>Combinação de 8 a 25 letras(maiúsculas/minúsculas), números e símbolos sem espaços.</p>
                 </div>
                 <div>
-                    <label>Confirme sua senha</label>
+            
                     <input className={styles.divInput} type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirme sua senha" />
                 </div>
+                <div className={styles.checkbox}><label htmlFor="">
+                    <input className={styles.checkbox}type="checkbox" name="acceptTerms" id="acceptTerms" />
+                    Li e aceitei a Política de privacidade
+                </label>
+                </div>
+
                 <input className={styles.submit} type="button" value="Registrar" />
+                <p className={styles.rodape}>IESB - BAY AREA</p>
             </section>
         </main>
     )
