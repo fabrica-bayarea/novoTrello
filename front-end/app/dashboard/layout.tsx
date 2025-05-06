0039d8f7288d2dd3bdced09668c69769342c0abb
Arthur Rabelo <arthur.rabel@outlook.com>
2025-05-06 20:54:19 -0300

    feat: implement dashboard ui

--- front-end/app/dashboard/layout.tsx
+++ front-end/app/dashboard/layout.tsx
@@ -1,10 +1,28 @@
-import styles from "./style.module.css";
+import { Bell, CircleHelp } from "lucide-react";
 import Image from 'next/image';
+import Link from 'next/link';
+import styles from "./style.module.css";
 
 export default function
  AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className={styles.main}>
+        <header className={styles.header}>
+            <div className={styles.wrapper_header}>
+                <Image
+                    className={styles.logoContainer}
+                    src="/images/iesb-icon.png" 
+                    alt="Logo IESB" 
+                    width={100}
+                    height={100}
+                />
+                <div className={styles.wrapper_header_helps}>
+                    <Bell size={40} color="#949494" strokeWidth={2} />
+                    <CircleHelp size={40} color="#949494" strokeWidth={2} />
+                    <Link href="dashboard/edit-profile/" className={styles.profileImage}></Link>
+                </div>
+            </div>
+        </header>
         <Image
             className={styles.cornerBottomLeft}
             src="/images/auth-background-bottom.png" 
