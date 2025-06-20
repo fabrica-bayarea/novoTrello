70742735612aaf7e56000c442532ad4b600a14f3
Arthur Rabelo <arthur.rabel@outlook.com>
2025-06-19 21:17:33 -0300

    build: implement CI

--- back-end/test/jest-e2e.config.ts
+++ back-end/test/jest-e2e.config.ts
@@ -1,18 +1,18 @@
 import { Config } from 'jest';
 import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
+import { compilerOptions } from '../tsconfig.json';
 
 const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testRegex: '.*\\.spec.ts$',
+  rootDir: '../.',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
-  testEnvironment: 'node',
 };
 
 export default config;
