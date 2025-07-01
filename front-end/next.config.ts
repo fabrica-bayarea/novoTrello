46a6d5386c0275b25879541cca13487e3211326b
Arthur Rabelo <arthur.rabel@outlook.com>
2025-07-01 19:57:28 -0300

    chore(security): update CSP

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  poweredByHeader: false,
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self';
+      style-src 'self' https://fonts.googleapis.com;
+      img-src 'self' data:;
+      font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com;
+      connect-src 'self';
+      frame-ancestors 'self';
+      upgrade-insecure-requests;
+      form-action 'self';
+      base-uri 'self';
+      object-src 'none';
+    `.replace(/\s{2,}/g, ' ').trim();
+
+    return [
+      {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
